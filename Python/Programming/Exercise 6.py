43333d9b4cd930f1db6739190b208c2591a2f206
Seyyed Mohammad Hadi IzadKhah <smhi76@yahoo.com>
2023-05-13 00:14:58 +0330

    Recent Updates.

---
+++ Python/Programming/Exercise 6.py
@@ -0,0 +1,26 @@
+# A program that gets a number and prints if the number odd or even.
+def odd_even(number = None):
+    if number != None:
+        if number % 2 == 1:
+            print('Number' , number , 'is Odd!')
+        else:
+            print('Number' , number , 'is Even!')
+    else:
+        number = input('Enter a number:\n')
+        number = int(number)
+        if number % 2 == 1:
+            print('Number' , number , 'is Odd!')
+        else:
+            print('Number' , number , 'is Even!')
+
+if __name__ == '__main__':
+    odd_even()
+    odd_even(4)
+    odd_even(18)
+    odd_even(49)
+    odd_even(80)
+    odd_even(100)
+    odd_even(47)
+    for i in range(20):
+        odd_even((3 * i) + 1)
+        odd_even((5 * i) - (2 * i) + 1)
